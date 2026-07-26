4ae349a52076118e0f17cbd493fdeac7bf58377d
FaiXal <114605188+Allrounder687@users.noreply.github.com>
2026-07-26 23:54:31 +0530

    feat: update .gitignore, add APK upload automation, and include new Android resources

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,6 +36,16 @@ android {
             signingConfig = signingConfigs.getByName("release")
         }
     }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val outputFileName = "IT-Feels-v${variant.versionName}.apk"
+                output.outputFileName = outputFileName
+            }
+    }
 }
 
 kotlin {
