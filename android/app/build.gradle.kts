f5f0d5f92a0aeee962cf77f0bf989eaa625ab356
FaiXal <114605188+Allrounder687@users.noreply.github.com>
2026-07-26 15:23:52 +0530

    Design new app icon and update release configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,21 +15,25 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.pixelplayer.saavn.pixel_player_saavn"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "key"
+            keyPassword = "android"
+            storeFile = file("key.jks")
+            storePassword = "android"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
