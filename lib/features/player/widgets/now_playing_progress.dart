0a5dfde4e1f557ab2a69fcc53a2da694a54c871c
Achie <cleverfox687@gmail.com>
2026-08-05 02:34:44 +0530

    fix(player): add missing riverpod_bridge imports

--- lib/features/player/widgets/now_playing_progress.dart
+++ lib/features/player/widgets/now_playing_progress.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:it_feels_music/core/providers/riverpod_bridge.dart';
 import 'package:it_feels_music/core/theme/theme_ext.dart';
 import 'package:it_feels_music/features/player/audio_player_provider.dart';
 import 'package:it_feels_music/features/player/video_player_provider.dart';
